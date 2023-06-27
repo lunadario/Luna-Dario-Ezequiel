cd7db64f725807b64570b29275a6150e53591f94
luna dario <lunabau148@gmail.com>
2023-06-26 23:17:10 -0300

    elimine carpeta prueba

--- pruebaFront/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-export default function Card(props) {
-    return (
-      <div className='card'>
-        <h2>Nombre: {props.nombre}</h2>
-        <h2> Apellido: {props.apellido}</h2>
-      </div>
-    )
-  }
-  
